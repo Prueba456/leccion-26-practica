37019128a73cc583d96d1ec4c369916bdbfe409a
Betsi Loayza <betsi18_12@hotmail.com>
2017-04-28 13:14:00 -0500

    random confirmacion

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
+var texto = document.getElementById("random");
+window.addEventListener("load", function (e){
+  e.preventDefault();
+  var caracteres = "abcdefghijkmnpqrtuvwxyzABCDEFGHIJKLMNPQRTUVWXYZ123456789´";
+  var contrasena = "";
+  for (var i=0; i<6; i++) {
+  contrasena += caracteres.charAt(Math.floor(Math.random()*caracteres.length));
+  }
+  return texto.value = contrasena;
+});
+  var comprobar = document.getElementById("comparar");
+  var enviar = document.getElementById("check");
+  enviar.addEventListener("click",function(){
 
+  if(comprobar.value == texto.value){
+  alert("Comprobacion correcta");
+  } else{
+  alert("Intente otra vez");
+  }
+});
+  var recargar = document.getElementById("reload");
+  recargar.addEventListener("click",function(){
+   location.reload();
+  });
